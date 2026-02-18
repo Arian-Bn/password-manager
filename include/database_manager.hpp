86d42d319eb0a10248d2982be981db9efb7d3ba3
Arian-Bn <arianbagheri2476@gmail.com>
2026-02-18 06:26:25 +0000

    Add basic structure in database_manager.hpp

--- include/database_manager.hpp
+++ include/database_manager.hpp
@@ -2,4 +2,13 @@
 #include <sqlite3.h>
 #include <string>
 
-class DatabaseManager {};
+class DatabaseManager {
+public:
+  DatabaseManager(const std::string &db_path);
+  ~DatabaseManager();
+  bool initialize();
+
+private:
+  sqlite3 *db_;
+  std::string db_path_;
+};
