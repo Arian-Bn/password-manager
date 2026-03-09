a28a4de076db33f83817ccc34c92af3e5c518d27
Arian-Bn <arianbagheri2476@gmail.com>
2026-03-09 22:45:36 +0000

    Refactor Vault: remove in-memory vector, add DatabaseManager pointer

--- include/vault.hpp
+++ include/vault.hpp
@@ -1,12 +1,11 @@
 #pragma once
-#include "entry.hpp"
+#include "database_manager.hpp"
 #include <memory>
 #include <string>
-#include <vector>
 
 class Vault {
 public:
-  Vault() = default;
+  Vault();
   ~Vault() = default;
 
   void addPasswordEntry(const std::string &title, const std::string &website,
@@ -15,23 +14,12 @@ public:
   void addNoteEntry(const std::string &title, const std::string &content);
 
   void displayAll() const;
-
   void findByTitle(const std::string &title) const;
 
-  size_t getEntryCount() const { return entries_.size(); }
-
+  size_t getEntryCount() const;
   bool deleteEntry(size_t index);
-
   void clear();
 
-  const std::vector<std::unique_ptr<Entry>> &getAllEntries() const {
-    return entries_;
-  }
-
-  void addEntry(std::unique_ptr<Entry> entry) {
-    entries_.push_back(std::move(entry));
-  }
-
 private:
-  std::vector<std::unique_ptr<Entry>> entries_;
+  std::unique_ptr<DatabaseManager> dbManager;
 };
