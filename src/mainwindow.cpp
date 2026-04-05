25047d626a89bedb83e55b51caff315454fc31af
Arian-Bn <arianbagheri2476@gmail.com>
2026-04-05 05:01:27 +0000

    feat: ad button slots with QMessageBox placeholders

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.hpp"
 #include <QGridLayout>
+#include <QMessageBox>
 #include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
@@ -16,10 +17,37 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
   deleteButton = new QPushButton("Delete");
   entryList = new QListWidget();
 
+  connect(addPasswordButton, &QPushButton::clicked, this,
+          &MainWindow::onAddPasswordClicked);
+  connect(addNoteButton, &QPushButton::clicked, this,
+          &MainWindow::onAddNoteClicked);
+  connect(deleteButton, &QPushButton::clicked, this,
+          &MainWindow::onDeleteClicked);
+
   // Arranging in a grid
   QGridLayout *layout = new QGridLayout(central);
   layout->addWidget(addPasswordButton, 0, 0);
   layout->addWidget(addNoteButton, 0, 1);
   layout->addWidget(deleteButton, 0, 2);
   layout->addWidget(entryList, 1, 0, 1, 3);
+}
+
+void MainWindow::onAddPasswordClicked() {
+  QMessageBox::information(this, "Add Password",
+                           "Password entry form will be here");
+}
+
+void MainWindow::onAddNoteClicked() {
+  QMessageBox::information(this, "Add Note", "Note entry form will be here");
+}
+
+void MainWindow::onDeleteClicked() {
+  QMessageBox::StandardButtons reply;
+  reply = QMessageBox::question(
+      this, "Delete", "Are you sure you want to delete the selected entry?",
+      QMessageBox::Yes | QMessageBox::No);
+
+  if (reply == QMessageBox::Yes) {
+    QMessageBox::information(this, "Delete", "Entry deleted (placeholder)");
+  }
 }
