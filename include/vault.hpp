ee293e3b80b3cf8efebcbe1d37ee73d93f03c300
Arian-Bn <arianbagheri2476@gmail.com>
2026-04-27 23:05:32 +0000

    refactor: simplify Vault to note-only interface

--- include/vault.hpp
+++ include/vault.hpp
@@ -8,15 +8,7 @@ public:
   Vault();
   ~Vault() = default;
 
-  void addPasswordEntry(const std::string &title, const std::string &website,
-                        const std::string &username,
-                        const std::string &password);
   void addNoteEntry(const std::string &title, const std::string &content);
-
-  void displayAll() const;
-  void findByTitle(const std::string &title) const;
-
-  size_t getEntryCount() const;
   bool deleteEntry(int id);
   void clear();
 
