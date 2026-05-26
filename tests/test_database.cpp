02b5d3ecb6556a8d7517e7e67a7bc6d11bccab81
Arian-Bn <arianbagheri2476@gmail.com>
2026-05-26 17:17:55 +0400

    debug: temporarily break test to see CI fail

--- tests/test_database.cpp
+++ tests/test_database.cpp
@@ -23,7 +23,7 @@ TEST_F(DatabaseTest, AddNote) {
 
   auto entries = db->getAllEntries();
   EXPECT_EQ(entries.size(), 1);
-  EXPECT_EQ(std::get<1>(entries[0]), "Test Title");
+  EXPECT_EQ(std::get<1>(entries[0]), "Wrong Title");
 }
 
 TEST_F(DatabaseTest, DeleteNote) {
